479cc997234a138ec3cf0ed9bd0eb8b1f4188dc8
Mohammad Shoaib <mohammadshoaib1366@gmail.coom>
2024-11-18 15:59:54 +0530

    add CRUD ,reqConstuctor --shoaib

--- CrudOperation007/src/main/java/com/nit/service/StudentServiceImpl.java
+++ CrudOperation007/src/main/java/com/nit/service/StudentServiceImpl.java
@@ -1,5 +1,7 @@
 package com.nit.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,6 +16,30 @@ public class StudentServiceImpl implements IStudent {
 	
 	@Override
 	public String createAccount(Student stu) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Student> showAllDatails() throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Student getStudentByName(String name) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String updateStudent(Integer id) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String deleteStudentById(Integer id) throws Exception {
 		// TODO Auto-generated method stub
 		return null;
 	}
