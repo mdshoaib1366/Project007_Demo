415e2ede028ad1f299f8060d164f93fae9d1e0f9
Shuja <Shuja@DESKTOP-667F718>
2024-11-18 15:38:22 +0530

    service and entity --- Shuja

---
+++ CrudOperation007/src/main/java/com/nit/entity/Student.java
@@ -0,0 +1,22 @@
+package com.nit.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name = "Stdu")
+@Data
+
+public class Student {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer roll;
+	private String name;
+	private String addr;
+	private Double fee;
+}
---
+++ CrudOperation007/src/main/java/com/nit/service/IStudent.java
@@ -0,0 +1,10 @@
+package com.nit.service;
+
+import com.nit.entity.Student;
+
+
+
+public interface IStudent {
+
+	public String createAccount(Student stu);
+}
